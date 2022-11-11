b86fa467e650cae449f42dda760f752efc7d1b23
Dylan Arlich <d.arlich42@gmail.com>
2022-11-10 20:55:12 -0800

    add initial arithmetic logic and html boilerplate

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
+function add(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    return "Only type numbers, plz";
+  } else return a + b;
+}
 
+function subtract(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    return "Only type numbers, plz";
+  } else return a - b;
+}
+
+function multiply(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    return "Only type numbers, plz";
+  }
+  return a * b;
+}
+
+function divide(a, b) {
+  if (typeof a != "number" || typeof b != "number") {
+    return "Only type numbers, plz";
+  } else if (b == 0) {
+    return "Impossible!";
+  } else return a / b;
+}
