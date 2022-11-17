0f9678d69334ed2f2e567249cf7c057dbbe70027
Dylan Arlich <d.arlich42@gmail.com>
2022-11-17 12:31:28 -0800

    finish functions for full calculator to provide answers

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let currentNum = "";
 let previousNum = "";
 let operator = "";
+let answer = "";
 
 const currentDisplay = document.querySelector(".current");
 const previousDisplay = document.querySelector(".previous");
@@ -8,9 +9,8 @@ const clear = document.querySelector(".clear-btn");
 const equals = document.querySelector(".equals");
 const numbers = document.querySelectorAll(".calc-btn");
 const operators = document.querySelectorAll(".operator");
-//functions to display numbers clicked in display
-//WILL ADD SOMETHING TO STORE NUMBER VALUE!!!!
 
+//functions to display numbers clicked in display
 numbers.forEach((number) =>
   number.addEventListener("click", function (e) {
     handleNumber(e.target.textContent);
@@ -24,10 +24,45 @@ function handleNumber(num) {
   }
 }
 
+//function to clear display
 clear.addEventListener("click", function (e) {
   currentDisplay.textContent = "";
   currentNum = "";
+  operator = "";
+  previousNum = "";
+  answer = "";
 });
+
+//function to make operator store in variable and display
+operators.forEach((op) =>
+  op.addEventListener("click", function (e) {
+    operator = e.target.textContent;
+    currentDisplay.textContent = operator;
+    previousNum = currentNum;
+    currentNum = "";
+  })
+);
+
+//evaluates answer
+equals.addEventListener("click", function (e) {
+  answer = operate(Number(previousNum), operator, Number(currentNum));
+
+  currentDisplay.textContent = Math.round(answer * 1000000) / 1000000;
+  currentNum = answer;
+});
+
+//function to coallesce arithmetic function into one place
+function operate(num1, op, num2) {
+  if (op === "+") {
+    return add(num1, num2);
+  } else if (op === "-") {
+    return subtract(num1, num2);
+  } else if (op === "*") {
+    return multiply(num1, num2);
+  } else if (op === "/") {
+    return divide(num1, num2);
+  } else return "Operator Error";
+}
 
 //functions for basic arithmetic
 function add(a, b) {
@@ -56,15 +91,7 @@ function divide(a, b) {
     return "Impossible!";
   } else return a / b;
 }
-//function to coallesce arithmetic function into one place
-function operate(num1, operator, num2) {
-  if (operator === "+") {
-    return add(num1, num2);
-  } else if (operator === "-") {
-    return subtract(num1, num2);
-  } else if (operator === "*") {
-    return multiply(num1, num2);
-  } else if (operator === "/") {
-    return divide(num1, num2);
-  } else return "Operator Error";
-}
+
+console.log(previousNum);
+console.log(operator);
+console.log(currentNum);
